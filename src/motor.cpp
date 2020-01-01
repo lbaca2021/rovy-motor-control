530be95bed36b8d9d5ff11155633c2d4e659a034
Daniel <daniel@ubuntu-18.04>
2020-01-01 00:11:14 -0800

    better speed control via keyboard

--- src/motor.cpp
+++ src/motor.cpp
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <math.h>
 #include <map>
+#include <termios.h>
 
 #include <wiringPi.h>
 #include <mcp23017.h>
@@ -31,6 +32,41 @@ static bool orientationThreadInit = false;
 static double speed = 0;
 static map<int, int> speedMapping;
 static pthread_mutex_t speedLock = PTHREAD_MUTEX_INITIALIZER; // @suppress("Invalid arguments")
+
+//**************** terminal adjustments (read key without RETURN) ***************************
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  current = old; /* make new settings same as old settings */
+  current.c_lflag &= ~ICANON; /* disable buffered i/o */
+  if (echo) {
+      current.c_lflag |= ECHO; /* set echo mode */
+  } else {
+      current.c_lflag &= ~ECHO; /* set no echo mode */
+  }
+  tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character */
+char getch(void)
+{
+	  char ch;
+	  initTermios(0);
+	  ch = getchar();
+	  resetTermios();
+	  return ch;
+}
+//**********************************************
+
 
 void buildSpeedMapping() {
 	double a = 15.0654;
@@ -169,6 +205,7 @@ void *speedControlThread(void *vargp) {
 void terminate(int s){
 	driverVoltageOff();
 	i2c0Unlock();
+	resetTermios();
 	exit(0);
 }
 
@@ -218,41 +255,35 @@ int main(int argc, char *argv[]) {
 	motorsStop();
 	driverVoltageOn();
 
-//	int c;
-//	do {
-//		c = getchar();
-//		switch (c) {
-//		case 'w':
-//	    	pthread_mutex_lock(&speedLock);
-//	    	speed += 100;
-//	    	pthread_mutex_unlock(&speedLock);
-//			break;
-//		case 's':
-//	    	pthread_mutex_lock(&speedLock);
-//	    	speed -= 100;
-//	    	pthread_mutex_unlock(&speedLock);
-//			break;
-//		case ' ':
-//			motorsStop();
-//	    	pthread_mutex_lock(&speedLock);
-//	    	speed = 0;
-//	    	pthread_mutex_unlock(&speedLock);
-//			break;
-//		}
-//	} while (c != '.');
-
-	int s;
+	char c;
 	do {
-		cin >> s;
-		if (s == 0) motorsStop();
-		pthread_mutex_lock(&speedLock);
-		speed = s;
-		pthread_mutex_unlock(&speedLock);
+		do c = getch(); while (c == 27 || c == 91);
+		switch (c) {
+		case 68:
+			//left
+			break;
+		case 67:
+			//right
+			break;
+		case 65:
+			//up
+			pthread_mutex_lock(&speedLock);
+			speed += 100;
+			pthread_mutex_unlock(&speedLock);
+			break;
+		case 66:
+			//down
+			pthread_mutex_lock(&speedLock);
+			speed -= 100;
+			pthread_mutex_unlock(&speedLock);
+			break;
+		case ' ':
+			//stop
+			motorsStop();
+			pthread_mutex_lock(&speedLock);
+			speed = 0;
+			pthread_mutex_unlock(&speedLock);
+			break;
+		}
 	} while (1);
-
-	stopSpeedControlThread = true;
-	motorsStop();
-	driverVoltageOff();
-
-	cout << "done!" << endl;
 }
